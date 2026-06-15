cc3e47962440efb6b52164fe0cf1422d5d6d18e2
mortalswordsmanturn <kerensultan53@gmail.com>
2026-06-15 06:35:19 -0400

    Create Screenshot Framer/Document Window/Content View Controller/Help Controller/LanguageController.swift

---
+++ Screenshot Framer/Document Window/Content View Controller/Help Controller/LanguageController.swift
@@ -0,0 +1,46 @@
+//
+//  LanguageController.swift
+//  Screenshot Framer
+//
+//  Created by Patrick Kladek on 12.12.17.
+//  Copyright © 2017 Patrick Kladek. All rights reserved.
+//
+
+import Cocoa
+
+
+final class LanguageController {
+
+    // MARK: - Properties
+
+    let fileCapsule: FileCapsule
+
+    // MARK: - Lifecycle
+
+    init(fileCapsule: FileCapsule) {
+        self.fileCapsule = fileCapsule
+    }
+
+    // MARK: - LanguageController
+
+    func allLanguages(prefered: String? = nil) -> [String] {
+        let fileManager = FileManager()
+        guard let projectURL = self.fileCapsule.projectURL else { return [] }
+        guard let contents = try? fileManager.contentsOfDirectory(at: projectURL, includingPropertiesForKeys: nil, options: [.skipsSubdirectoryDescendants, .skipsHiddenFiles]) else { return [] }
+
+        let allLanguages = contents.filter { file in
+            var isDir = ObjCBool(false)
+            fileManager.fileExists(atPath: file.path, isDirectory: &isDir)
+            return isDir.boolValue
+        }.compactMap { $0.lastPathComponent }
+
+        let blackList = ["backgrounds", "device_frames", "export"]
+        let filteredLanguages = allLanguages.subtracting(blackList, caseSensitive: false)
+        if let prefered = prefered {
+            if filteredLanguages.contains(where: { $0.caseInsensitiveCompare(prefered) == .orderedSame }) {
+                return [prefered]
+            }
+        }
+        return filteredLanguages
+    }
+}
