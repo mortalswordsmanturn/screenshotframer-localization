417210c53fec78adf515f5d016f2c197d39569fa
mortalswordsmanturn <kerensultan53@gmail.com>
2026-06-15 06:35:33 -0400

    Create Screenshot Framer/Document Window/DocumentWindowController.swift

---
+++ Screenshot Framer/Document Window/DocumentWindowController.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentWindowController.swift
+//  Framer
+//
+//  Created by Patrick Kladek on 28.11.17.
+//  Copyright © 2017 Patrick Kladek. All rights reserved.
+//
+
+import Cocoa
+
+
+final class DocumentWindowController: NSWindowController, NSWindowDelegate {
+
+    // MARK: - Overrides
+
+    override var windowNibName: NSNib.Name? {
+        return String(describing: type(of: self))
+    }
+
+    override func windowDidLoad() {
+        self.shouldCloseDocument = true
+
+        // swiftlint:disable:next force_cast
+        let contentViewController = ContentViewController(document: self.document as! Document)
+        self.contentViewController = contentViewController
+    }
+}
