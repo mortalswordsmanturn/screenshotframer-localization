d51d6ad6f0be08b257ccc5e832d84c4783a0e224
mortalswordsmanturn <kerensultan53@gmail.com>
2026-06-15 06:35:02 -0400

    Create Screenshot Framer CLI/main.swift

---
+++ Screenshot Framer CLI/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Screenshot Framer CLI
+//
+//  Created by Patrick Kladek on 29.12.17.
+//  Copyright © 2017 Patrick Kladek. All rights reserved.
+//
+
+import ArgumentParser
+
+struct ScreenshotFramer: ParsableCommand {
+
+    static let configuration = CommandConfiguration(
+        abstract: "A Swift command-line tool to frame localised screenshots for the AppStore",
+        version: "2.0.1",
+        subcommands: [Export.self, Website.self])
+
+    init() { }
+}
+
+ScreenshotFramer.main()
